ed15651f8e6488b5488b965634494b4b9bb0feec
Alfred Victoria <aetv9302@gmail.com>
2024-07-16 20:53:31 +0800

    Enabled viewbinding; Commented out recycler view temporarily

--- app/src/main/java/com/example/questifyv1/MainActivity.java
+++ app/src/main/java/com/example/questifyv1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.questifyv1;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -8,6 +10,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.questifyv1.databinding.ActivityMainBinding;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,16 +21,25 @@ public class MainActivity extends AppCompatActivity {
     private PostAdapter postAdapter;
     private List<Post> postList;
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        // RecyclerView Stuff
+        /*
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         postList = DataHelper.getPostData();
         postAdapter = new PostAdapter(this, postList);
         recyclerView.setAdapter(postAdapter);
+
+         */
     }
 }
