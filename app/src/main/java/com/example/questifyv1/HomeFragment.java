d27597adb260a4405ffa7dfda25dd3ff7f3d5dee
Alfred Victoria <aetv9302@gmail.com>
2024-07-17 10:27:07 +0800

    Moved recyclerview from activity_main.xml to fragment_home.xml

--- app/src/main/java/com/example/questifyv1/HomeFragment.java
+++ app/src/main/java/com/example/questifyv1/HomeFragment.java
@@ -3,10 +3,14 @@ package com.example.questifyv1;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +25,10 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
+
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
     private String mParam1;
     private String mParam2;
 
@@ -60,5 +68,16 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        postList = DataHelper.getPostData();
+        postAdapter = new PostAdapter(this.getContext(),postList);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.setAdapter(postAdapter);
     }
 }
