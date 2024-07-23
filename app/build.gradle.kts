af87a6955809de0b5dbc9a9fa4305bcfb078ad8f
Alfred Victoria <aetv9302@gmail.com>
2024-07-23 15:08:30 +0800

    Bug: Fixed unresolved gradle bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
