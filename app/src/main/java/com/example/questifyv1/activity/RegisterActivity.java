958d4aa4cec8d6138c5cd05c46153c7a83277917
Alfred Victoria <aetv9302@gmail.com>
2024-07-23 10:05:35 +0800

    Function: Sign in and register navigate to each other

--- app/src/main/java/com/example/questifyv1/activity/RegisterActivity.java
+++ app/src/main/java/com/example/questifyv1/activity/RegisterActivity.java
@@ -1,6 +1,8 @@
-package com.example.questifyv1;
+package com.example.questifyv1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.questifyv1.R;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,15 @@ public class RegisterActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // TODO: Implement reverse swipe animation
+        // Navigate back to Sign In
+        btnBack = findViewById(R.id.btnSignIn);
+        btnBack.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this,SignInActivity.class);
+            startActivity(intent);
+        finish();
         });
     }
 }
--- app/src/main/java/com/example/questifyv1/activity/SignInActivity.java
+++ app/src/main/java/com/example/questifyv1/activity/SignInActivity.java
@@ -1,6 +1,8 @@
 package com.example.questifyv1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.questifyv1.R;
 
 public class SignInActivity extends AppCompatActivity {
+    private Button signInButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,20 @@ public class SignInActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signInButton = findViewById(R.id.btnSignIn);
+        signInButton.setOnClickListener(v -> {
+            // Handle sign-in button click
+        });
+
+        // Navigate to Register Activity
+        registerButton = findViewById(R.id.btnRegister);
+        registerButton.setOnClickListener(v -> {
+            // Handle register button click
+            Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
