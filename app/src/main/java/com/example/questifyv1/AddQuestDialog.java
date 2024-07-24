e1e4159b31c49d208c9e55b3da70b95ed149b766
Alfred Victoria <aetv9302@gmail.com>
2024-07-24 21:38:03 +0800

    Function: Integrated new date pickers

--- app/src/main/java/com/example/questifyv1/AddQuestDialog.java
+++ app/src/main/java/com/example/questifyv1/AddQuestDialog.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.BaseColumns;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -28,11 +27,14 @@ import com.example.questifyv1.activity.MainActivity;
 import com.example.questifyv1.database.QuestsDatabaseHandler;
 import com.example.questifyv1.database.QuestContract;
 
-import java.lang.reflect.Array;
-
 public class AddQuestDialog extends DialogFragment {
     private EditText etQuestTitle;
-    private EditText etDueDate;
+    private EditText etMonth;
+    private EditText etDay;
+    private EditText etYear;
+    private EditText etHour;
+    private EditText etMinute;
+    private Spinner spAmPm;
     private EditText etDesc;
     private EditText etNumReward;
     private Button btnPostQuest;
@@ -66,13 +68,27 @@ public class AddQuestDialog extends DialogFragment {
         // Initialize widgets
         etQuestTitle = view.findViewById(R.id.et_QuestTitle);
         spCategory = view.findViewById(R.id.et_Category);
-        etDueDate = view.findViewById(R.id.et_DueDate);
         etDesc = view.findViewById(R.id.et_Desc);
         etNumReward = view.findViewById(R.id.et_Reward);
         btnPostQuest = view.findViewById(R.id.btn_addQuest);
         btnClose = view.findViewById(R.id.btn_close);
+        // Initialize dates widgets
+        etMonth = view.findViewById(R.id.et_month);
+        etDay = view.findViewById(R.id.et_day);
+        etYear = view.findViewById(R.id.et_year);
+        etHour = view.findViewById(R.id.et_hour);
+        etMinute = view.findViewById(R.id.et_MM);
+        spAmPm = view.findViewById(R.id.sp_AMPM);
 
-        // Spinner Setup
+        // Date (am/pm) Spinner setup
+        ArrayAdapter<CharSequence> spAmPmAdapter = ArrayAdapter.createFromResource(
+                getActivity(),
+                R.array.date_categories,
+                R.layout.spinner_item
+        );
+        spAmPm.setAdapter(spAmPmAdapter);
+
+        // Category Spinner Setup
         ArrayAdapter<CharSequence> spAdapter = ArrayAdapter.createFromResource(
                 getActivity(),
                 R.array.quest_categories,
@@ -96,7 +112,10 @@ public class AddQuestDialog extends DialogFragment {
 
                 // Check if input fields are empty
                 if( etQuestTitle.getText().toString().isEmpty() ||
-                    etDueDate.getText().toString().isEmpty() ||
+                    etMonth.getText().toString().isEmpty() ||
+                    etDay.getText().toString().isEmpty() ||
+                    etYear.getText().toString().isEmpty() ||
+                    etHour.getText().toString().isEmpty() ||
                     etDesc.getText().toString().isEmpty() ||
                     etNumReward.getText().toString().isEmpty()
                     ){
@@ -162,7 +181,16 @@ public class AddQuestDialog extends DialogFragment {
                         ContentValues values = new ContentValues();
                         values.put(QuestContract.QuestEntry.COLUMN_NAME_TITLE, etQuestTitle.getText().toString());
                         values.put(QuestContract.QuestEntry.COLUMN_NAME_CATEGORY, spCategory.getSelectedItem().toString());
-                        values.put(QuestContract.QuestEntry.COLUMN_NAME_DUE_DATE, etDueDate.getText().toString());
+
+                        // Build date string from et and sp
+                        String dateString =
+                                etMonth.getText().toString() + "/" +
+                                etDay.getText().toString() +  "/" +
+                                etYear.getText().toString() + " " +
+                                "@ " + etHour.getText().toString() +
+                                ":" + etMinute.getText().toString() +
+                                " " + spAmPm.getSelectedItem().toString();
+                        values.put(QuestContract.QuestEntry.COLUMN_NAME_DUE_DATE, dateString);
                         values.put(QuestContract.QuestEntry.COLUMN_NAME_DESCRIPTION, etDesc.getText().toString());
                         values.put(QuestContract.QuestEntry.COLUMN_NAME_REWARD, etNumReward.getText().toString());
                         values.put(QuestContract.QuestEntry.COLUMN_NAME_STATUS, "NONE");
