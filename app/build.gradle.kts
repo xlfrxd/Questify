0b52a6ca106e8086df4170f072ea9afd98cbb675
Alfred Victoria <141266092+itscaicai@users.noreply.github.com>
2024-08-06 12:36:40 +0800

    Updated package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.questifyv1"
+        applicationId = "com.questify.apk"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
